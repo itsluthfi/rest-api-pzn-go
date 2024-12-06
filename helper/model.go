f46ea1bba247da2e4d57e1e6346af793116d964d
Luthfi Izzuddin Hanif <luthfi.izzuddin1@gmail.com>
2024-12-06 19:44:12 +0700

    Create and implement category service

---
+++ helper/model.go
@@ -0,0 +1,21 @@
+package helper
+
+import (
+	"rest-api-pzn-go/model/domain"
+	"rest-api-pzn-go/model/web"
+)
+
+func ToCategoryResponse(category domain.Category) web.CategoryResponse {
+	return web.CategoryResponse{
+		Id:   category.Id,
+		Name: category.Name,
+	}
+}
+
+func ToCategoryResponses(categories []domain.Category) []web.CategoryResponse {
+	var categoryResponses []web.CategoryResponse
+	for _, category := range categories {
+		categoryResponses = append(categoryResponses, ToCategoryResponse(category))
+	}
+	return categoryResponses
+}
