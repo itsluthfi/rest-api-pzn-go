33dd30896c78281387ffd0fdd79464a950be3d8b
Luthfi Izzuddin Hanif <luthfi.izzuddin1@gmail.com>
2024-12-10 16:09:33 +0700

    Create HTTP router

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"rest-api-pzn-go/app"
+	"rest-api-pzn-go/controller"
+	"rest-api-pzn-go/repository"
+	"rest-api-pzn-go/service"
+
+	"github.com/go-playground/validator/v10"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/julienschmidt/httprouter"
+)
+
+func main() {
+
+	db := app.NewDB()
+	validate := validator.New()
+	categoryRepository := repository.NewCategoryRepository()
+	categoryService := service.NewCategoryService(categoryRepository, db, validate)
+	categoryController := controller.NewCategoryController(categoryService)
+
+	router := httprouter.New()
+
+	router.GET("/api/categories", categoryController.FindAll)
+	router.GET("/api/categories/:categoryId", categoryController.FindById)
+	router.POST("/api/categories", categoryController.Create)
+	router.PUT("/api/categories/:categoryId", categoryController.Update)
+	router.DELETE("/api/categories/:categoryId", categoryController.Delete)
+
+}
