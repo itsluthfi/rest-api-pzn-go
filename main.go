46aab713dad6a7e28326a5458584b3a58a917234
Luthfi Izzuddin Hanif <luthfi.izzuddin1@gmail.com>
2024-12-11 14:55:07 +0700

    Create and implement middleware

--- main.go
+++ main.go
@@ -5,12 +5,12 @@ import (
 	"rest-api-pzn-go/app"
 	"rest-api-pzn-go/controller"
 	"rest-api-pzn-go/helper"
+	"rest-api-pzn-go/middleware"
 	"rest-api-pzn-go/repository"
 	"rest-api-pzn-go/service"
 
 	"github.com/go-playground/validator/v10"
 	_ "github.com/go-sql-driver/mysql"
-	"github.com/julienschmidt/httprouter"
 )
 
 func main() {
@@ -20,18 +20,11 @@ func main() {
 	categoryRepository := repository.NewCategoryRepository()
 	categoryService := service.NewCategoryService(categoryRepository, db, validate)
 	categoryController := controller.NewCategoryController(categoryService)
-
-	router := httprouter.New()
-
-	router.GET("/api/categories", categoryController.FindAll)
-	router.GET("/api/categories/:categoryId", categoryController.FindById)
-	router.POST("/api/categories", categoryController.Create)
-	router.PUT("/api/categories/:categoryId", categoryController.Update)
-	router.DELETE("/api/categories/:categoryId", categoryController.Delete)
+	router := app.NewRouter(categoryController)
 
 	server := http.Server{
 		Addr:    "localhost:3000",
-		Handler: router,
+		Handler: middleware.NewAuthMiddleware(router),
 	}
 
 	err := server.ListenAndServe()
---
+++ middleware/auth_middleware.go
@@ -0,0 +1,33 @@
+package middleware
+
+import (
+	"net/http"
+	"rest-api-pzn-go/helper"
+	"rest-api-pzn-go/model/web"
+)
+
+type AuthMiddleware struct {
+	Handler http.Handler
+}
+
+func NewAuthMiddleware(handler http.Handler) *AuthMiddleware {
+	return &AuthMiddleware{Handler: handler}
+}
+
+func (middleware *AuthMiddleware) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
+	if "RAHASIA" == request.Header.Get("X-API-Key") {
+		// ok
+		middleware.Handler.ServeHTTP(writer, request)
+	} else {
+		// error
+		writer.Header().Set("Content-Type", "application/json")
+		writer.WriteHeader(http.StatusUnauthorized)
+
+		webResponse := web.WebResponse{
+			Code:   http.StatusUnauthorized,
+			Status: "UNAUTHORIZED",
+		}
+
+		helper.WriteToResponseBody(writer, webResponse)
+	}
+}
